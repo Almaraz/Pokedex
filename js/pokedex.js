c7426296ad9d3b7f6fb0895911a9534c8420ae14
Almaraz <ivan.almz.r@gmail.com>
2024-06-03 17:10:46 -0600

    Array of images

--- js/pokedex.js
+++ js/pokedex.js
@@ -11,9 +11,9 @@ function setDescription(text) {
 }
 
 function loader(isLoading = false) {
-    const $screen = document.querySelector('#screen')
-    const img = isLoading ? 'url(./images/loading.gif)' : '' 
-    $screen.style.backgroundImage = img
+	const $screen = document.querySelector('#screen')
+	const img = isLoading ? 'url(./images/loading.gif)' : ''
+	$screen.style.backgroundImage = img
 }
 
 export async function findPokemon(id) {
@@ -22,20 +22,36 @@ export async function findPokemon(id) {
 	const description = species.flavor_text_entries.find(
 		(flavor) => flavor.language.name === 'es'
 	)
-    return {
-        sprites: pokemon.sprites.front_default,
-        description: description.flavor_text,
-        id: pokemon.id
-    }
+	const sprites = [pokemon.sprites.front_default]
+
+	for (const item in pokemon.sprites) {
+		//Not null
+		if (
+			item !== 'front_default' &&
+			item !== 'other' &&
+			item !== 'versions' &&
+			pokemon.sprites[item]
+		) {
+			sprites.push(pokemon.sprites[item]) // push all sprites to array
+		}
+	}
+
+	console.log(sprites)
+
+	return {
+		sprites,
+		description: description.flavor_text,
+		id: pokemon.id,
+	}
 }
 
-export async function setPokemon (id){
-    //Encender loader
-    loader(true)
-    const pokemon = await findPokemon(id)
-    //Apagar Loader
-    loader(false)
-    setImage(pokemon.sprites)
-    setDescription(pokemon.description)
-    return pokemon
+export async function setPokemon(id) {
+	//Encender loader
+	loader(true)
+	const pokemon = await findPokemon(id)
+	//Apagar Loader
+	loader(false)
+	setImage(pokemon.sprites[0])
+	setDescription(pokemon.description)
+	return pokemon
 }
